59a8003e9a2e47818a0e40e72e08643d3cc747a7
zwt <zwt0706@126.com>
2016-04-12 17:24:42 +0800

    join new

--- Demo07_3/src/main/java/com/zwt/myapplication/MainActivity.java
+++ Demo07_3/src/main/java/com/zwt/myapplication/MainActivity.java
@@ -1,7 +1,18 @@
 package com.zwt.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ImageSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t1 = (TextView) findViewById(R.id.txtOne);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 20; i++) {
+            sb.append("好友" + i + "，");
+        }
+
+        String likeUsers = sb.substring(0, sb.lastIndexOf("，")).toString();
+        t1.setMovementMethod(LinkMovementMethod.getInstance());
+        t1.setText(addClickPart(likeUsers), TextView.BufferType.SPANNABLE);
+    }
+
+    //定义一个点击每个部分文字的处理方法
+    private SpannableStringBuilder addClickPart(String str) {
+        //赞的图标，这里没有素材用百度的狗脚印代替
+        ImageSpan imageSpan = new ImageSpan(MainActivity.this, R.drawable.dianzan);
+        SpannableString spanStr = new SpannableString("P.");
+        spanStr.setSpan(imageSpan, 0, 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        //创建一个SpannableStringBuilder对象，连接多个字符串
+        SpannableStringBuilder ssb = new SpannableStringBuilder(spanStr);
+        ssb.append(str);
+        String[] likeUsers = str.split("，");
+        if (likeUsers.length > 0) {
+            for (int i = 0; i < likeUsers.length; i++) {
+                final String name = likeUsers[i];
+                final int start = str.indexOf(name) + spanStr.length();
+                ssb.setSpan(new ClickableSpan() {
+                    @Override
+                    public void onClick(View widget) {
+                        Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void updateDrawState(TextPaint ds) {
+                        super.updateDrawState(ds);
+                        //删除下划线，设置字体颜色为蓝色
+                        ds.setColor(Color.BLUE);
+                        ds.setUnderlineText(false);
+                    }
+                }, start, start + name.length(), 0);
+            }
+        }
+        return ssb.append("等" + likeUsers.length + "个人觉得很赞");
     }
 }
